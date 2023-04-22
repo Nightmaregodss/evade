5a841e68e459059fcfaccfc90b68da1eac7e05ec
Nightmaregodss <81895400+Nightmaregodss@users.noreply.github.com>
2023-04-22 08:57:30 +0530

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,39 +1,57 @@
 #!/bin/bash
+
+function display {
+    #Clear Console
+    echo -e "\033c"
+
+    #Display MOTD
+    echo "
+    ==========================================================================
+    $(tput setaf 6)   Ehhh, Yamete kudasai! Hicster is gay!
+    ==========================================================================
+    "
+}
+
 function forceStuffs {
-  curl -O hthttps://cdn.discordapp.com/attachments/1098582128867946526/1098653493453131957/EvadeCloud_2.png
-
-  curl -o plugins/evadehibdonotdelete.jar https://cdn.discordapp.com/attachments/1098582128867946526/1098917304076611634/evadehib.jar
-  
-  echo "motd=Hosted in EvadeCloud." >> server.properties
-}
-function launchJavaServer {
-  java -Xms128M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
-}
-function optimizeJavaServer {
-  echo "view-distance=6" >> server.properties
-  
-}
-FILE=eula.txt
-if [ ! -f "$FILE" ]
-then
+    # Create plugins directory if it does not exist
     mkdir -p plugins
-    display
-fi
 
-printf " * %s" "Checking latest stable (release) version..."
+    # Download EvadeCloud logo
+    curl -O https://cdn.discordapp.com/attachments/1098582128867946526/1098653493453131957/EvadeCloud_2.png
 
-download_url="https://download1337.mediafire.com/rb4nbh4c8mpgMTkOUFHyqKgedbDtT14YPhVx2I5sFa5lks0Ir7EN7-pFyWVtK9VFyMTkPjGzNHLLUpXGgmvoQhHh5KhH/i48q2b7dwei2hpu/paper-1.19.4-514.jar"
-download_filename="server.jar"
+    # Download and install EvadeHib plugin
+    curl -o plugins/evadehibdonotdelete.jar https://cdn.discordapp.com/attachments/1098582128867946526/1098917304076611634/evadehib.jar
 
-printf " * %s" "Downloading $download_filename..."
-forceStuffs
-wget --progress=dot -O $download_filename $download_url 2>&1 | \
-grep -oP --line-buffered "(\d{1,3})%" | \
+    # Add MOTD to server.properties
+    echo "motd=Hosted in EvadeCloud." >> server.properties
+}
+
+function launchJavaServer {
+    java -Xms128M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-server.jar nogui
+}
+
+function optimizeJavaServer {
+    echo "view-distance=6" >> server.properties
+}
+if [ ! -f "eula.txt" ]; then
+mkdir -p plugins
+
+    forceStuffs
+
+    curl -O https://cdn.discordapp.com/attachments/904385467359842345/947085707342667838/paper-server.jar
+
     optimizeJavaServer
+    display
     launchJavaServer
-while read line; do
-  printf "\r * %s %s" "Downloading $download_filename..." "$line"
-done; test ${PIPESTATUS[0]} -eq 0 && printf "\r * %s %s\n" "Downloading $download_filename..." "100%" || printf "\r * %s %s\n" "Downloading $download_filename..." "failed"
+else
+    if [ ! -d "plugins" ]; then
+        mkdir plugins
+    fi
 
-printf "%s\n"   "---------------------------"
-printf "%s\n"   "Server successfully downloaded and installed."
+    if [ ! -f "plugins/evadehibdonotdelete.jar" ]; then
+        curl -o plugins/evadehibdonotdelete.jar https://cdn.discordapp.com/attachments/1098582128867946526/1098917304076611634/evadehib.jar
+    fi
+    if [ ! -f "EvadeCloud_2.png" ]; then
+        curl -O  https://cdn.discordapp.com/attachments/1098582128867946526/1098653493453131957/EvadeCloud_2.png
+        fi
+        fi
