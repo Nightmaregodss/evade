25e79f23376596c0598f192a72f96a6ade8d2469
Nightmaregodss <81895400+Nightmaregodss@users.noreply.github.com>
2023-09-14 18:34:24 +0530

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -8,6 +8,10 @@ function forceStuffs {
     echo "motd=Hosted in Myridax. Fun fact of the week: The host has been running smooth since Demon, CR072 and Kns joined the team." >> server.properties
 }
 
+function launchJavaServer {
+    java -Xms128M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
+}
+
 function optimizeJavaServer {
     echo "view-distance=6" >> server.properties
 }
@@ -18,15 +22,11 @@ curl -O https://cdn.discordapp.com/attachments/1102995352421933147/1150080021218
 
 optimizeJavaServer
 
-function launchJavaServer {
-    java -Xms128M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
-}
-
 launchJavaServer
     if [ ! -d "plugins" ]; then
         mkdir plugins
     fi
-    if [ ! -f "plugins/HibernateX.jar" ]; then
+    if [ ! -f "HibernateX.jar" ]; then
         curl plugins/HubernareX.jar -O https://raw.githubusercontent.com/ewn69/hicstr-hibernate/main/assets/HibernateX.jar
     fi
     if [ ! -f "server-icon.png" ]; then
