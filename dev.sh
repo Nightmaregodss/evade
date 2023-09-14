0fe729563a53dbc018c26117bb33e940f2f5ef6a
Nightmaregodss <81895400+Nightmaregodss@users.noreply.github.com>
2023-09-14 18:21:28 +0530

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -3,15 +3,9 @@
 function forceStuffs {
     mkdir -p plugins 
 
-    curl -o https://media.discordapp.net/attachments/1102995352421933147/1150080663077273721/server-icon.png
-    
-    curl -o plugins/hibernationmadebyexpress.jar https://raw.githubusercontent.com/ewn69/hicstr-hibernate/main/assets/HibernateX.jar
+    curl -O https://media.discordapp.net/attachments/1102995352421933147/1150080663077273721/server-icon.png
     
     echo "motd=Hosted in Myridax. Fun fact of the week: The host has been running smooth since Demon, CR072 and Kns joined the team." >> server.properties
-}
-
-function launchJavaServer {
-    java -Xms128M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
 }
 
 function optimizeJavaServer {
@@ -24,14 +18,17 @@ curl -O https://cdn.discordapp.com/attachments/1102995352421933147/1150080021218
 
 optimizeJavaServer
 
+function launchJavaServer {
+    java -Xms128M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
+}
+
 launchJavaServer
     if [ ! -d "plugins" ]; then
         mkdir plugins
     fi
-
-    if [ ! -f "hibernationmadebyexpress.jar" ]; then
-        curl -o https://raw.githubusercontent.com/ewn69/hicstr-hibernate/main/assets/HibernateX.jar
+    if [ ! -f "Hibernatex.jar" ]; then
+        curl -O https://raw.githubusercontent.com/ewn69/hicstr-hibernate/main/assets/HibernateX.jar
     fi
     if [ ! -f "server-icon.png" ]; then
-        curl -O  https://media.discordapp.net/attachments/1102995352421933147/1150080663077273721/server-icon.png?width=616&height=616
+        curl -O  https://media.discordapp.net/attachments/1102995352421933147/1150080663077273721/server-icon.png
         fi
